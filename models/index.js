78a8e9b2934344eba5ae27224b73e3f62c4302f2
seraleman <seraleman1204@gmail.com>
2022-11-11 19:45:38 -0500

    Se implementa la creación de una noticia
    - Se agregan validaciones a la ruta incluyendo la de JWT y rol administrador

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Product = require('./product')
 const Role = require('./role')
 const User = require('./user')
 const Invoice = require('./invoice')
+const Novelty = require('./novelty')
 
 module.exports = {
   Category,
@@ -10,4 +11,5 @@ module.exports = {
   Role,
   User,
   Invoice,
+  Novelty,
 }
