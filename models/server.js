71fce2b41a1a2372b6bd9947f66eb53ef81f6aed
Valen <lorenamontoya2004@gmail.com>
2022-11-13 15:15:23 -0500

    Iniciando la creación del modelo 'Articles'

--- models/server.js
+++ models/server.js
@@ -17,6 +17,7 @@ class Server {
       uploads: '/api/uploads',
       users: '/api/users',
       invoices: '/api/invoices',
+      articles: '/api/articles',
     }
 
     // Conectar a base datos
@@ -61,6 +62,7 @@ class Server {
     this.app.use(this.paths.uploads, require('../routes/upload.routes'))
     this.app.use(this.paths.users, require('../routes/user.routes'))
     this.app.use(this.paths.invoices, require('../routes/invoice.routes'))
+    this.app.use(this.paths.articles, require('../routes/article.routes'))
   }
 
   listen() {
