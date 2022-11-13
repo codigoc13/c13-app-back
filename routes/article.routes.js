2eee51373830da7ec5521eb6123bdc2e789112bd
LozanoTheStain <adslozanov@gmail.com>
2022-11-13 16:25:24 -0500

    Iniciando la creación del controlador de articles

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -2,6 +2,8 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 
 const { validateFields } = require('../middlewares')
+const { createArticle } = require('../controllers/article.controller')
+
 
 const router = Router() 
 
