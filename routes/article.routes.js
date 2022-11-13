c5df5e9968b4adeed294c75ebfa0b5d18eb99e9a
LozanoTheStain <adslozanov@gmail.com>
2022-11-13 17:10:17 -0500

    Creando validaciones del modelo 'Article'
    Dimos a conocer su eistencia en el index y en el modelo hemos quitado los 'param' que no queremos mostrar

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -1,22 +1,25 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 
-const { validateFields } = require('../middlewares')
+const { validateFields, isRole } = require('../middlewares')
 const { createArticle } = require('../controllers/article.controller')
 
 
 const router = Router() 
 
 router.post('/', 
-[
-
+[   
+    isRole('ADMIN_ROLE'),
+    check('title', 'El titulo es requerido para poder crear el articulo').not().isEmpty,
+    check('description', 'La descripción es necesaria').not().isEmpty(),
+    validateFields,
 ],
 createArticle)
 
-router.get('/', createArticle)
+// router.get('/', findAllArticle)
 
-router.put('/:id', [], createArticle)
+// router.put('/:id', [], updateArticle)
 
-router.delete('/:id', [], createArticle)
+// router.delete('/:id', [], deleteArticle)
 
 module.exports = router
