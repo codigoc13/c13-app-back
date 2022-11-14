c30b7771a6a34d1b8348f0a40bf95d6ed0151fd6
milenasandra <zsandra745@gmail.com>
2022-11-14 16:32:48 -0500

    Se crean las rutas get y post

--- models/server.js
+++ models/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 
-const { dbConnection } = require('../database/config.db')
+const {dbConnection} = require('../database/config.db')
 
 class Server {
   constructor() {
@@ -17,6 +17,7 @@ class Server {
       uploads: '/api/uploads',
       users: '/api/users',
       invoices: '/api/invoices',
+      cohorts: '/api/cohorts',
     }
 
     // Conectar a base datos
@@ -61,6 +62,7 @@ class Server {
     this.app.use(this.paths.uploads, require('../routes/upload.routes'))
     this.app.use(this.paths.users, require('../routes/user.routes'))
     this.app.use(this.paths.invoices, require('../routes/invoice.routes'))
+    this.app.use(this.paths.cohorts, require('../routes/cohort.routes'))
   }
 
   listen() {
