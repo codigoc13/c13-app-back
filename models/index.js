6006522d734e943b553e7c1968dc67237ed26ba7
Panditax0902 <juanchos2905@gmail.com>
2022-11-14 23:12:56 -0500

    Se agregan modelos, rutas y controladores por completo

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Product = require('./product')
 const Role = require('./role')
 const User = require('./user')
 const Invoice = require('./invoice')
+const Course = require('./course')
 
 module.exports = {
   Category,
@@ -10,4 +11,5 @@ module.exports = {
   Role,
   User,
   Invoice,
+  Course,
 }
