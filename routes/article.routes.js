2531c29eb17fed7e03d9a0d3b1c639c457968b5d
LozanoTheStain <adslozanov@gmail.com>
2022-11-15 18:47:13 -0500

    Crear usuario y listar usuarios implementado

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -2,39 +2,52 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 
 const { isRole, validateJWT } = require('../middlewares')
-const {validateFields} = require('../middlewares/validate-fields')
-const { createArticle, findAllArticles, updateArticle, deleteArticle } = require('../controllers/article.controller')
+const { validateFields } = require('../middlewares/validate-fields')
+const {
+  createArticle,
+  findAllArticles,
+  updateArticle,
+  deleteArticle,
+} = require('../controllers/article.controller')
 
-const router = Router() 
+const router = Router()
 
-router.post('/', 
-[   
+router.post(
+  '/',
+  [
     validateJWT,
     isRole('ADMIN_ROLE'),
-    check('title', 'El titulo es requerido para poder crear el articulo').not().isEmpty,
-    check('description', 'La descripción es necesaria').not().isEmpty(),
+    check('title', 'El titulo es requerido').not().isEmpty(),
+    check('description', 'La descripción es requerida').not().isEmpty(),
     validateFields,
-],
-createArticle)
+  ],
+  createArticle
+)
 
 router.get('/', findAllArticles)
 
-router.put('/:id', 
-[
-    validateJWT,
-    check('id', 'El Id no es valido').isMongoId(),
-    check('title', 'El titulo es requerido para la actualizacíon').not().isEmpty(),
-    check("description", "La descripción es necesaria").not().isEmpty(),
-validateFields,
-], updateArticle)
+// router.put(
+//   '/:id',
+//   [
+//     validateJWT,
+//     check('id', 'El Id no es valido').isMongoId(),
+//     check('title', 'El título es requerido').not().isEmpty(),
+//     check('description', 'La descripción es necesaria').not().isEmpty(),
+//     validateFields,
+//   ],
+//   updateArticle
+// )
 
-router.delete('/:id', 
-[
-    validateJWT,
-    isRole('ADMIN_ROLE'),
-    check('id', 'El id no es valido').isMongoId,
-    // check('id').custom(articleByIdExists),
-    validateFields,
-], deleteArticle)
+// router.delete(
+//   '/:id',
+//   [
+//     validateJWT,
+//     isRole('ADMIN_ROLE'),
+//     check('id', 'El id no es valido').isMongoId,
+//     // check('id').custom(articleByIdExists),
+//     validateFields,
+//   ],
+//   deleteArticle
+// )
 
 module.exports = router
