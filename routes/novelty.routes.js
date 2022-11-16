d801c1e95a4d33f0b755201bac319c730b2507d1
seraleman <seraleman1204@gmail.com>
2022-11-15 20:47:32 -0500

    Se implementa eliminación suave de noticia

--- routes/novelty.routes.js
+++ routes/novelty.routes.js
@@ -3,7 +3,12 @@ const { check } = require('express-validator')
 
 const { validateJWT, isRole } = require('../middlewares')
 const { validateFields } = require('../middlewares/validate-fields')
-const { create, findAll, update } = require('../controllers/novelty.controller')
+const {
+  create,
+  findAll,
+  update,
+  deleteById,
+} = require('../controllers/novelty.controller')
 const { noveltytByIdExists } = require('../helpers/db-validators')
 
 const router = Router()
@@ -19,6 +24,8 @@ router.post(
   create
 )
 
+router.get('/', findAll)
+
 router.patch(
   '/:id',
   [
@@ -31,6 +38,16 @@ router.patch(
   update
 )
 
-router.get('/', findAll)
+router.delete(
+  '/:id',
+  [
+    validateJWT,
+    isRole('ADMIN_ROLE'),
+    check('id', 'El id debe ser válido de Mongo').isMongoId(),
+    check('id').custom(noveltytByIdExists),
+    validateFields,
+  ],
+  deleteById
+)
 
 module.exports = router
