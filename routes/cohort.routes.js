d2b5197adf4298cc34ea34570f9f8bd7b261caea
JordanRendon <jordanre7991@gmail.com>
2022-11-18 21:09:06 -0500

    creado get, post, update de cohortes

--- routes/cohort.routes.js
+++ routes/cohort.routes.js
@@ -1,7 +1,10 @@
-const {Router} = require('express')
-const {check} = require('express-validator')
-const {createCohort, getCohorts} = require('../controllers/cohort.controller')
-const {validateFields, validateJWT, isRole} = require('../middlewares')
+const { Router } = require('express')
+const { check } = require('express-validator')
+const { createCohort, getCohorts, updateCohort } = require('../controllers/cohort.controller')
+const { cohortByIdExists } = require('../helpers/db-validators')
+const { validateParticipants } = require('../middlewares/validate-participants')
+const { validateFields, validateJWT, isRole } = require('../middlewares')
+const { validateCareers } = require('../middlewares/validate-careers')
 
 const router = Router()
 
@@ -9,15 +12,27 @@ router.get('/', getCohorts)
 
 router.post(
   '/',
-  [ 
+  [
     validateJWT,
     isRole('ADMIN_ROLE'),
     check('code', 'El código es obligatorio').not().isEmpty(),
     check('duration', 'La duración es obligatoria').not().isEmpty(),
     check('quantity', 'La cantidad es obligatoria').not().isEmpty(),
+    validateCareers,
+    validateParticipants,
     validateFields,
   ],
   createCohort
 )
+
+router.patch('/:id', [
+  validateJWT,
+  isRole('ADMIN_ROLE'),
+  check('id', 'El ID no es válido').isMongoId(),
+  check('id').custom(cohortByIdExists),
+  validateCareers,
+  validateParticipants,
+  validateFields,
+], updateCohort)
 
 module.exports = router
