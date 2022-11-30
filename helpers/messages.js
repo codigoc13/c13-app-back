19966822a18bee77b465ca96c8b58089cd2dca5e
lorenamontoya2004@gmail.com <adslozanov@gmail.com>
2022-11-30 10:00:33 -0500

    Terminando factorización de articles y la carga de imágenes del mismo

--- helpers/messages.js
+++ helpers/messages.js
@@ -1,5 +1,9 @@
 const message = {
   idIsNotValid: 'El ID debe ser válido de Mongo',
+  requireTitle: 'El título es requerido',
+  requireDescription: 'La descripción es requerida',
+  stringTitle: 'El título debe ser una cadena de caracteres',
+  stringDescription: 'La descripción debe ser una cadena de caracteres',
 }
 
 module.exports = {
